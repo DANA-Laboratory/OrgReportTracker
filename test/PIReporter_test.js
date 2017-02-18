7114990b432baf568a8dc2689c601a020647e608
reza afzalan <rafzalan@gmail.com>
2017-02-18 14:16:16 +0330

    jshint

--- test/PIReporter_test.js
+++ test/PIReporter_test.js
@@ -3,6 +3,7 @@
 //var PIReporter = require('../lib/PIReporter.js');
 var config = require('config');
 var assert = require('assert');
+//var modelsSqlite3 = require('../lib/models-sqlite3');
 describe('DataBase', function() {
   describe('Configuration', function() {
     it('should exists: dbPath', function() {
