095d099291d5faac3685a85b4f33d69ec2f62387
reza afzalan <rafzalan@gmail.com>
2017-02-18 14:47:13 +0330

    test db create

--- test/PIReporter_test.js
+++ test/PIReporter_test.js
@@ -1,13 +1,23 @@
 'use strict';
-
-//var PIReporter = require('../lib/PIReporter.js');
+var fs = require('fs');
 var config = require('config');
 var assert = require('assert');
-//var modelsSqlite3 = require('../lib/models-sqlite3');
 describe('DataBase', function() {
   describe('Configuration', function() {
-    it('should exists: dbPath', function() {
+    it('should exists: dbPath', function(done) {
       assert(config.get('dbPath'));
+      done();
+    });
+  });
+  describe('Db Create', function() {
+    before(function () {
+        if (fs.existsSync(config.get('dbPath'))) {
+            fs.unlinkSync(config.get('dbPath'));
+        }
+    });
+    it('should create database', function(done) {
+      var modelsSqlite3 = require('../lib/models-sqlite3');
+      modelsSqlite3.createDB().then(() => done()).catch((err)=>console.log(err));
     });
   });
 });
