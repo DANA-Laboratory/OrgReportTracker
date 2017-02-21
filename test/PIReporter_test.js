f5900e622e39e12f13a5eba750c39aad4f47b046
Reza Afzalan <rafzalan@gmail.com>
2017-02-22 00:24:56 +0330

    testing insert

--- test/PIReporter_test.js
+++ test/PIReporter_test.js
@@ -1,7 +1,11 @@
 'use strict';
-var fs = require('fs');
-var config = require('config');
-var assert = require('assert');
+const fs = require('fs');
+const config = require('config');
+const assert = require('assert');
+const modelsSqlite3 = require('../lib/models-sqlite3');
+const app = require('../');
+const supertest = require('supertest')(app);
+
 describe('DataBase', function() {
   describe('Configuration', function() {
     it('should exists: dbPath', function(done) {
@@ -16,8 +20,25 @@ describe('DataBase', function() {
         }
     });
     it('should create database', function(done) {
-      var modelsSqlite3 = require('../lib/models-sqlite3');
-      modelsSqlite3.createDB().then(() => done()).catch((err)=>console.log(err));
+      modelsSqlite3.createDB(modelsSqlite3.ddl).then(() => done()).catch((err)=>console.log(err));
     });
+  });
+});
+describe('Api test', function() {
+  it('get home', function(done) {
+      supertest
+          .get('/')
+          .expect(200)
+          .end(done);
+  });
+  it('addUser', function(done) {
+      let data  = '{"verb":"addUser", "data":{"workunit":"un1", "pcode":"555555", "account":"rafzalan", "password":"vafa01", "fname":"رضا", "lname":"افضلان"}}';
+      supertest
+          .post('/insert')
+          .type('json')
+          .send(data)
+          .expect('{"id":1}')
+          .expect(200)
+          .end(done);
   });
 });
