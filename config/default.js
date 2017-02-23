134ac8c30f71812fdbc5fafce80d17b3212c0b69
Reza Afzalan <rafzalan@gmail.com>
2017-02-23 14:14:29 +0330

    without secrets

--- config/default.js
+++ config/default.js
@@ -1,8 +1,17 @@
+var secrets = {
+  "GITHUB_CLIENT_ID": 'random',
+  "GITHUB_CLIENT_SECRET": 'random',
+  "APP_ID" : 'random',
+  "APP_SECRET" : 'random'
+};
+if (require('fs').existsSync('./secret.secret')) {
+  secrets = require('./secret.secret');
+}
 module.exports = {
     "couldCreateAdmin": false,
     "session": true,
-    "GITHUB_CLIENT_ID": require('./secret.secret').GITHUB_CLIENT_ID,
-    "GITHUB_CLIENT_SECRET": require('./secret.secret').GITHUB_CLIENT_SECRET,
-    "APP_ID" : require('./secret.secret').APP_ID,
-    "APP_SECRET" : require('./secret.secret').APP_SECRET
+    "GITHUB_CLIENT_ID": secrets.GITHUB_CLIENT_ID,
+    "GITHUB_CLIENT_SECRET": secrets.GITHUB_CLIENT_SECRET,
+    "APP_ID" : secrets.APP_ID,
+    "APP_SECRET" : secrets.APP_SECRET
 }
