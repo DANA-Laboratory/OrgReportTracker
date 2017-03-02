10e1e6a77de6dbcb91fc2770c5957052da6ed3b3
Reza Afzalan <rafzalan@gmail.com>
2017-03-02 19:23:56 +0330

    structuring file

--- static/js/config/router-config.js
+++ static/js/config/router-config.js
@@ -1,5 +1,8 @@
 angular.module('PIR').config(function($routeProvider) {
   $routeProvider
+    .when("/pi", {
+        templateUrl : 'panels/pi.pug'
+    })
     .when("/report", {
         templateUrl : 'panels/openreport.pug'
     })
