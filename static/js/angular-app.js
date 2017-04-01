5faece8b1f01ebba2f09389a44adafa30794b099
dana-laboratory <rafzalan@gmail.com>
2017-04-01 23:49:26 +0430

    side-nav tempelting

--- static/js/angular-app.js
+++ static/js/angular-app.js
@@ -1,1 +1,18 @@
-var app = angular.module('PIR', ['ui.grid', 'ui.grid.edit', 'ui.grid.cellNav', 'ngRoute', 'chart.js']);
+var app = angular.module('PIR', ['ui.grid', 'ui.grid.edit', 'ui.grid.cellNav', 'ngRoute', 'chart.js', 'ui.bootstrap']);
+app.directive("searchresult", function() {
+    return {
+        link: function(scope, el, attrs) {
+          if(scope.results === undefined) {
+            scope.results = {};
+          }
+          scope.results[attrs.searchQuery] = [
+            {
+              text: attrs.searchQuery
+            },
+            {
+              text: attrs.searchQuery
+            }
+          ]
+        }
+    };
+});
