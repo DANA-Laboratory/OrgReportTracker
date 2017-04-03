d849f0bd1e1f3090d239eb29fb467f514909a8a0
reza afzalan <rafzalan@gmail.com>
2017-04-03 15:19:46 +0430

    before replace table names

--- static/js/angular-app.js
+++ static/js/angular-app.js
@@ -1,4 +1,4 @@
-var app = angular.module('PIR', ['ui.grid', 'ui.grid.edit', 'ui.grid.cellNav', 'ngRoute', 'chart.js', 'ui.bootstrap']);
+var app = angular.module('PIR', ['ui.grid', 'ui.grid.edit', 'ui.grid.cellNav', 'ngRoute', 'chart.js', 'ui.bootstrap', 'ngResource']);
 app.directive("searchresult", function() {
     return {
         link: function(scope, el, attrs) {
@@ -15,4 +15,18 @@ app.directive("searchresult", function() {
           ]
         }
     };
+});
+
+['user', 'reportClass', 'variableCat_1', 'variableCat_2', 'variableCat_3', 'variableCat_1', 'variableDef', 'report', 'variable', 'attachement', 'value', 'target', 'message']
+.forEach((urlobject)=>{
+  app.factory(urlobject, ['$resource',
+    function($resource) {
+      return $resource(`/restful/${urlobject}`, {}, {
+        get: {method: 'GET', cache: false, isArray: false},
+        save: {method: 'POST', cache: false, isArray: false},
+        update: {method: 'PUT', cache: false, isArray: false},
+        delete: {method: 'DELETE', cache: false, isArray: false}
+      });
+    }]
+  );
 });
