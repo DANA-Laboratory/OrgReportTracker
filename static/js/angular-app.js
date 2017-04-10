1b40f34f231534e9816e586b94e6e919c94e8141
reza afzalan <rafzalan@gmail.com>
2017-04-10 16:31:22 +0430

    get log

--- static/js/angular-app.js
+++ static/js/angular-app.js
@@ -17,7 +17,7 @@ app.directive("searchresult", function() {
     };
 });
 
-['user', 'reportClass', 'variableCat_1', 'variableCat_2', 'variableCat_3', 'variableCat_1', 'variableDef', 'report', 'variable', 'attachement', 'value', 'target', 'message']
+['user', 'reportClass', 'variableCat_1', 'variableCat_2', 'variableCat_3', 'variableCat_1', 'variableDef', 'report', 'variable', 'reportVariable', 'attachement', 'value', 'target', 'message']
 .forEach((urlobject)=>{
   app.factory(urlobject, ['$resource',
     function($resource) {
@@ -29,6 +29,32 @@ app.directive("searchresult", function() {
       });
     }]
   );
+});
+
+['log']
+.forEach((urlobject)=>{
+  app.factory(urlobject, ['$resource',
+    function($resource) {
+      return $resource(`/restful/${urlobject}`, {}, {
+          'get':    {method:'GET'},
+          'save':   {method:'POST'},
+          'query':  {method:'GET', isArray:true},
+          'remove': {method:'DELETE'},
+          'delete': {method:'DELETE'}
+      });
+    }]
+  );
+});
+
+['log']
+.forEach((urlobject)=>{
+  app.controller(urlobject + 'Controller',['$scope', urlobject, function ($scope, urlobject) {
+      $scope.test = function () {
+          var res = urlobject.get({}, function() {
+            console.log(res);
+          });
+      }
+  }]);
 });
 
 app.controller('scopeUpdater', function ($scope) {
