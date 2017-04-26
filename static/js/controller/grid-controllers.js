da9ede864cb5e66c1a38f2dd7808969914783ef0
dana-laboratory <rafzalan@gmail.com>
2017-04-26 23:48:04 +0430

    numeric type for grid cell

--- static/js/controller/grid-controllers.js
+++ static/js/controller/grid-controllers.js
@@ -5,13 +5,13 @@ angular.module('PIR').controller('report-grid', ['$scope', '$http', function ($s
       columnDefs: [
         { name:fa['commands'], enableCellEdit:false, enableFiltering: false, cellTemplate: '<div class="ui-grid-cell-contents text-center"><a href="#" class="anchor-grid fa fa-info fa-fw"></a><a href="#" class="anchor-grid fa fa-line-chart fa-fw"></a><a href="#" class="anchor-grid fa fa-comments fa-red fa-fw" style="color:orange"></a><a href="#" class="anchor-grid fa fa-flag fa-fw"></a></div>'},
         { name:fa['pi'], field: 'PI', enableCellEdit:false },
-        { name:fa['pi real'], field: 'PI real' },
-        { name:fa['pi category'], field: 'PI category' },
-        { name:fa['pi weight'], field: 'PI weight' },
-        { name:fa['pi target'], field: 'PI target' },
+        { name:fa['pi real'], field: 'PI real', type:'number' },
+        { name:fa['pi category'], field: 'PI category'},
+        { name:fa['pi weight'], field: 'PI weight', type:'number' },
+        { name:fa['pi target'], field: 'PI target', type:'number' },
         { name:fa['unit'], field: 'Unit', enableCellEdit:false},
-        { name:fa['lower limit'], field: 'Lower Limit', enableCellEdit:false},
-        { name:fa['upper limit'], field: 'Upper Limit', enableCellEdit:false},
+        { name:fa['lower limit'], field: 'Lower Limit', enableCellEdit:false, type:'number'},
+        { name:fa['upper limit'], field: 'Upper Limit', enableCellEdit:false, type:'number'},
         { name:fa['edit time'], field: 'Edit Time', enableCellEdit:false}
       ]
     };
@@ -27,7 +27,10 @@ angular.module('PIR').controller('cat-grid', ['$scope', function ($scope) {
     $scope.init = function(masterField) {
         $scope.masterField = masterField;
         $scope.query($scope.callback);
-    }
+    };
+    $scope.$on('uiGridEventEndCellEdit', function (data) {
+        updatesum();
+    });
     $scope.gridOptions = {
       enableColumnMenus: false,
       enableFiltering: true,
@@ -36,7 +39,7 @@ angular.module('PIR').controller('cat-grid', ['$scope', function ($scope) {
         { name:fa['commands'], width: '5%', enableCellEdit:false, enableFiltering: false, cellTemplate: '<div class="ui-grid-cell-contents text-center"><a href="#" class="anchor-grid fa fa-remove fa-fw" style="color:red"></a></div>'},
         { name:fa['code'], width: '10%', field: 'code'},
         { name:fa['caption'], width: '50%', field: 'caption'},
-        { name:fa['weight'], width: '10%', field: 'weight'},
+        { name:fa['weight'], width: '10%', field: 'weight', type: 'number'},
         { name:fa['share'], width: '10%', field: 'share', enableCellEdit:false},
         { name:fa['attribute'], field: 'attribute', enableCellEdit:false},
       ]
@@ -50,7 +53,11 @@ angular.module('PIR').controller('cat-grid', ['$scope', function ($scope) {
       } else {
           $scope.gridOptions.data = data.filter($scope.filter);
       }
-      var sum = $scope.gridOptions.data.reduce((a,b)=>(a+b.weight), 0);
-      $scope.gridOptions.data.forEach((item)=>{item.share=item.weight/sum*100});
+      updatesum();
+    };
+    var updatesum = function() {
+      var sum = $scope.gridOptions.data.reduce((a,b)=>((b.weight) + (a)), 0);
+      console.log(sum);
+      $scope.gridOptions.data.forEach((item)=>{item.share = (item.weight/sum*100).toPrecision(4)});
     };
 }]);
