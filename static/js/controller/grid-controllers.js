ccde8b08fd94ed710d0eafb1659d4091860d259d
reza afzalan <rafzalan@gmail.com>
2017-05-29 11:17:44 +0430

    grid for reportClass

--- static/js/controller/grid-controllers.js
+++ static/js/controller/grid-controllers.js
@@ -22,7 +22,6 @@ angular.module('PIR').controller('report-grid', ['$scope', '$http', function ($s
     };
     $http.post('/data/report', {report: 'test'}).then(function(res){
       $scope.gridOptions.data = res.data;
-      // console.log($scope.gridOptions.data);
     }).catch(function(e){
       console.log(e);
       // handle errors in processing or in error.
@@ -33,6 +32,11 @@ angular.module('PIR').controller('cat-grid', ['$scope', function ($scope) {
     var urlobject = undefined;
     var masterKey = undefined;
     $scope.init = function(urlo, masterField_, handler) {
+        if (masterField_ === 'ReportClass') {
+            $scope.gridOptions.columnDefs = reportClasscolumnDefs;
+        } else {
+            $scope.gridOptions.columnDefs = columnDefs;
+        }
         masterField = masterField_;
         urlobject = urlo;
         masterKey = $scope.getlatestselected(masterField);
@@ -45,7 +49,6 @@ angular.module('PIR').controller('cat-grid', ['$scope', function ($scope) {
         if($scope.config_show_only_latest === true){
           masterKey = $scope.getlatestselected(masterField)
         }
-        console.log(masterKey)
         $scope.query(urlobject, {where: masterField.toLowerCase()+'_id', value: masterKey}, $scope.callback);
     };
     $scope.$on('uiGridEventEndCellEdit', function (data) {
@@ -55,15 +58,22 @@ angular.module('PIR').controller('cat-grid', ['$scope', function ($scope) {
       enableColumnMenus: false,
       enableFiltering: true,
       enableRowHashing:false,
-      columnDefs: [
-        { name:fa['commands'], width: '5%', enableCellEdit:false, enableFiltering: false, cellTemplate: '<div class="ui-grid-cell-contents text-center"><a href="#" class="anchor-grid fa fa-remove fa-fw" style="color:red"></a></div>'},
-        { name:fa['code'], width: '10%', field: 'code'},
-        { name:fa['caption'], width: '50%', field: 'caption'},
-        { name:fa['weight'], width: '10%', field: 'weight', type: 'number'},
-        { name:fa['share'], width: '10%', field: 'share', enableCellEdit:false},
-        { name:fa['attribute'], field: 'attribute', enableCellEdit:false},
-      ]
     };
+    var reportClasscolumnDefs = [
+            { name:fa['commands'], width: '5%', enableCellEdit:false, enableFiltering: false, cellTemplate: '<div class="ui-grid-cell-contents text-center"><a href="#" class="anchor-grid fa fa-remove fa-fw" style="color:red"></a></div>'},
+            { name:fa['code'], width: '10%', field: 'code', enableCellEdit:false},
+            { name:fa['caption'], width: '50%', field: 'caption', enableCellEdit:false},
+            { name:fa['VariableCat']['VariableCat_3'], width: '25%', field: 'variablecat_3_caption', enableCellEdit:false},
+            { name:fa['provider'], width: '10%', field: 'user_provider_account', enableCellEdit:false},
+    ];
+    var columnDefs = [
+            { name:fa['commands'], width: '5%', enableCellEdit:false, enableFiltering: false, cellTemplate: '<div class="ui-grid-cell-contents text-center"><a href="#" class="anchor-grid fa fa-remove fa-fw" style="color:red"></a></div>'},
+            { name:fa['code'], width: '10%', field: 'code'},
+            { name:fa['caption'], width: '50%', field: 'caption'},
+            { name:fa['weight'], width: '10%', field: 'weight', type: 'number'},
+            { name:fa['share'], width: '10%', field: 'share', enableCellEdit:false},
+            { name:fa['attribute'], field: 'attribute', enableCellEdit:false},
+    ];
     $scope.callback = function(data) {
       $scope.gridOptions.data = data;
       updatesum();
